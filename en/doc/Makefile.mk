23330111386ae5fef24db81ce08108c02a9d3824
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2024-09-29 10:30:31 +0200

    Updated make

--- en/doc/Makefile.mk
+++ en/doc/Makefile.mk
@@ -1,2 +1,2 @@
 $(FINAL_EN_DOC_PDF): $(EN_DOC_PARTS) $(EN_CONFIG_INPUT)
-	$(LATEXMK) -jobname=$(subst /,-,$(<D)) $(EN_DOC_DIR)/doc
+	$(LATEXMK) -jobname=$(subst /,-,$(<D))-coding $(EN_DOC_DIR)/doc
--- es/doc/Makefile.mk
+++ es/doc/Makefile.mk
@@ -1,2 +1,2 @@
 $(FINAL_ES_DOC_PDF): $(ES_DOC_PARTS) $(ES_CONFIG_INPUT)
-	$(LATEXMK) -jobname=$(subst /,-,$(<D)) $(ES_DOC_DIR)/doc
+	$(LATEXMK) -jobname=$(subst /,-,$(<D))-coding $(ES_DOC_DIR)/doc
